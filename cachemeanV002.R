a4e6d4991667ba55b4bad6b934414d6c973e3f08
JimCallahanOrlando <Jim.Callahan.Orlando@gmail.com>
2015-08-18 16:14:17 -0400

    Rewrote cachemeanV003 to create list vector, x

--- cachemeanV002.R
+++ cachemeanV002.R
@@ -7,9 +7,13 @@ makeVector <- function(x = numeric()) {
         x <<- y
         m <<- NULL
     }
+    
     get <- function() x
+    
     setmean <- function(mean) m <<- mean
+    
     getmean <- function() m
+    
     list(set = set, get = get,
          setmean = setmean,
          getmean = getmean)
---
+++ cachemeanV003.R
@@ -0,0 +1,54 @@
+# File: cachemeanV003.R (modified in attempt to fix "$" issue)
+#       This will attempt a more drastic solution -- embedding functions in list
+#       cachemean.R (example from class assignment: "Programming Assignment 2")
+
+# makeVector creates a list of functions for use by cachemean
+makeVector <- function()
+    
+    x <-- list(
+        set <- function(y) {
+            x <<- y
+            m <<- NULL
+            } ,
+        
+    
+        get <- function() x ,
+        
+    
+        setmean <- function(mean) m <<- mean ,
+    
+        getmean <- function() m 
+    
+        )
+
+cachemean <- function(x, ...) {
+    # cachemean: If available, use saved (cached) result, "m".
+    
+    # Attempt to retrieve saved result "m" 
+    # without reading passed data and performing computation.
+    m <- x$getmean()
+    
+    
+    # If m is FOUND (m is NOT NULL); RETURN SAVED (cached) "m" and EXIT.
+    if(!is.null(m)) {
+        message("getting cached data")
+        return(m)
+    }
+    
+    # If the program has reached this point
+    # m was NOT FOUND; get the passed data.
+    data <- x$get()
+    
+    # Compute the mean using the passed data.
+    m <- mean(data, ...)
+    
+    # Use function setmean() to persist "m" 
+    # (so it doesn't have to be calculated next call of cachemean)
+    x$setmean(m)
+    
+    # Print m
+    m
+}
+
+## End of: cachemeanV003.R
+    

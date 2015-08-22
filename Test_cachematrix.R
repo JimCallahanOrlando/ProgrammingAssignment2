8837e9df09bfb71867e41feae0744bb53ad2b808
JimCallahanOrlando <Jim.Callahan.Orlando@gmail.com>
2015-08-21 21:21:01 -0400

    Final Commit with working code

--- Test_cachematrix.R
+++ Test_cachematrix.R
@@ -4,21 +4,26 @@
 ## http://stackoverflow.com/questions/11995832/inverse-of-matrix-in-r
 
 # Build a sample matrix
-a <- rbind(c(1, -1/4), c(-1/4, 1))
+A <- as.matrix( rbind(c(1, -1/4), c(-1/4, 1) ) )
+# create an identity matrix, the same size as A
+identity <- diag( nrow = nrow(A) )
+B <- identity
 
 # Verify the object "a" is a matrix
-class(a)
+class(A)
+class(B)
 
 # Print the matrix
-a
+A
+B
 
 # Invert the matrix
-solve(a)
+X = solve(A,B)
 
 # Verify that inverse "matrix-multiplied" by original matrix gives an identity matrix
 # Identity matrix is all zeros, except for ones on the diagonal
 
-solve(a) %*%  a
+solve(A) %*%  A
 
 # NOTE: Must use " %*% " and NOT " * " because it must be "matrix multiplication"
 #       That is the subject of the StackOverFlow.com article above.
@@ -26,30 +31,21 @@ solve(a) %*%  a
 # Now that we have a matrix and an known inverse we can test the programs.
 
 # TEST2:
+# Load code
+source('~/GitHub/ProgrammingAssignment2/cachematrix.R', echo=FALSE)
+# Matrix Inverse for AX = B system of equations
+# the right-hand side(s) of the linear system. 
+# b is an identity matrix and solve will return the inverse of a.
+
 # Build a sample matrix
-a <- rbind(c(1, -1/4), c(-1/4, 1))
+A <- as.matrix( rbind(c(1, -1/4), c(-1/4, 1) ) )
 
-# Verify the object "a" is a matrix
-class(a)
+# Assign function library/list to "L"
+L <- makeCacheMatrix(A)
 
-# Print the matrix
-a
+# Request inverse (this is the first time so it is calcuated)
+X <- cacheSolve(A)
 
-# Assign the function to variable x.
-# VERY IMPORTANT -- MUST ASSIGN TO A VARIABLE.
-# DO NOT RUN makeCacheMatrix() on line by itself WITHOUT an assignment " <- ".
-xLib <- makeCacheMatrix(a)
+# Request inverse again (this is the second time so the stored copy is retrieved)
+X <- cacheSolve(A)
 
-# Verify that x is a list
-class(x)
-
-# Print the list x
-xLib
-
-xLib$get
-
-# 1st call: Invert the matrix
-cacheSolve(a)
-
-# 2nd call: Avoid inverting matrix by retrieving cached solution
-cacheSolve(a)
--- cachematrix.R
+++ cachematrix.R
@@ -3,61 +3,88 @@
 ##           if available, cachematrix uses a saved (cached) result, "m"
 ##           otherwise it calls solve() to compute a new solution.
 
-## INVOKE AS: a <- makeCacheMatrix() -- see examples at end of this file.
+## FOR DEMO ONLY:   Build a sample matrix
+#                   A <- as.matrix( rbind(c(1, -1/4), c(-1/4, 1) ) )
+
+## INVOKE AS:    L <- makeCacheMatrix(A)   # Assigns "list/library" of functions to "L"
+## followed by   X <- cacheSolve(A)
+#
+## where "L" is the name of the Library
+##       "A" is the input matrix (must be square)
+## and   "B" is an identity matrix the same size as "A" created by program
+## and   "X" is the results matrix (the inverse of "x")
+#
+## If "X" has been previously computed in the current session
+## wihtout any intervening calculations; "X" will be retrieved from the cache.
+## If "x" is not available for retrieval a solution will be computed
+## using the R solve() function.
 
 ## makeCacheMatrix() -- creates a list of functions for use by cacheSolve
-makeCacheMatrix <- function( x = matrix() ) {
+makeCacheMatrix <- function(A = as.matrix() ) {
     m <- NULL
     
-    ## 
-    set <- function(y) {
-        x <<- y
+    ## Store passed matrix in A.
+    set <- function(Z) {
+        A <<- Z
         m <<- NULL
     }
     
     ##
-    get <- function() x
+    get <- function() {
+        as.matrix(A)
+    }    
     
     ##
-    setSolve <- function(solve) m <<-solve
+    setSolve <- function(solve) {
+        m <<- NULL
+        m <<-solve
+    }
+        
     
     ##
     getSolve <- function() m
     
     ##
-    list(set = set, get = get,
-         setSolve = setSolve,
-         getSolve = getSolve)
-
+    
+    L   <-  list(set = set, 
+                 get = get,
+                 setSolve = setSolve,
+                 getSolve = getSolve)
+    
+    L   <- as.list(L) 
 }
 
 
 ## cacheSolve(x, ...) : Returns a matrix that is the inverse of 'x'
 
-cacheSolve <- function(x, ...) {
+cacheSolve <- function(A = as.matrix(), ...) {
     
     # Attempt to retrieve saved result "m" 
-    # without reading passed data and performing computation.    
-    m <- x$getSolve()
+    # without reading passed data and performing computation.
+    m <- L$getSolve()
     
     # If m is FOUND (m is NOT NULL); RETURN SAVED (cached) "m" and EXIT.
     if(!is.null(m)) {
             message("getting cached data")
-            return(m)
+            print(m)
+            return(as.matrix(m) )
     }
     
     # If the program has reached this point
     # m was NOT FOUND; get the passed data.
-    data <- x$get()
+    data <- L$get()
+    # create an identity matrix, the same size as A
+    identity <- diag( nrow = nrow(data) ) 
     
-    # Compute the inverse using the passed data.        
-    m <- solve(data, ...)
+    # Compute the inverse using the passed data and .        
+    m <- solve(data, identity, ...)
     
     ## Return a matrix that is the inverse of 'x'
-    x$setSolve(m)
+    L$setSolve(m)
     
     ## Print m
-    m
+    print(m)
+    return(as.matrix(m))
 }
 
 ### End of cachematrix.R
